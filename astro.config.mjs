da59e33ef22492507f654015cf62cbbd460c2e79
Otto <fa@ijust.cc>
2023-11-22 23:01:11 +0800

    style: run format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import qwikdev from "@qwikdev/astro";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [qwikdev(), tailwind()]
+  integrations: [qwikdev(), tailwind()],
 });
